45bac82ff9d89669845843168a90d37910a4dd30
Justin Rummel <j@justinrummel.com>
2013-02-20 11:37:44 -0500

    fixed reboot
    Reboot always "changed" the plist.  Doing a simple "touch" to check the
    version.

--- usr/local/bin/backProtect.sh
+++ usr/local/bin/backProtect.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 ### Variables
-day=`date '+%Y-%m-%d'`
+day=`date '+%Y-%m-%d_%H-%M-%S'`
 sourceDir="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/"
 destineDir="/Users/Shared/XProtect/"
 file="XProtect.plist"
-
+version=`/usr/libexec/PlistBuddy -c "print :Version" /System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/XProtect.meta.plist`
 
 [ ! -d "${destineDir}" ] && { mkdir "${destineDir}"; }
-cp "${sourceDir}${file}" "${destineDir}${day}-${file}"
+[ ! -e "${destineDir}".version/"${version}" ] && {
+	touch "${destineDir}".version/"${version}"
+	cp "${sourceDir}${file}" "${destineDir}${day}-${file}"
+}
+
